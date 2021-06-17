2d3e0b6af40d6ded511a0838f3ad46733e8845ab
Ramesh <ramesh@taashee.com>
2021-06-17 18:34:19 +0530

    first commit

---
+++ src/main/java/com/tml/springsoap/client/gen/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.tml.com/springsoap/gen", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.tml.springsoap.client.gen;
